b4003244107fb9499d342c3d12685b28713198a9
piccolojnr <rahimdaud246@gmail.com>
2024-02-24 21:51:16 +0000

    7. City

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,12 +8,14 @@ from models.state import State
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
 def get_states():
+    """get all states"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
 
 @app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def get_state(state_id):
+    """get state"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
@@ -22,6 +24,7 @@ def get_state(state_id):
 
 @app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
 def delete_state(state_id):
+    """delete state"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
@@ -32,6 +35,7 @@ def delete_state(state_id):
 
 @app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
+    """create state"""
     if not request.is_json:
         abort(400, description="Not a JSON")
     data = request.get_json()
@@ -44,6 +48,7 @@ def create_state():
 
 @app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def update_state(state_id):
+    """update state"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
