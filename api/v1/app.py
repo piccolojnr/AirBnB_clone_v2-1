6c1df89df9a6ebd445874f62cb9a659bc7157528
piccolojnr <rahimdaud246@gmail.com>
2024-02-24 18:51:02 +0000

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """api main file
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -16,6 +16,12 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 @app.teardown_appcontext
 def teardown_appcontext(error):
     storage.close()
+
+
+# Custom 404 handler
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
