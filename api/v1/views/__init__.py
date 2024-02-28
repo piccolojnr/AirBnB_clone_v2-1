356d98eeb0c821c5e01c02084342738f4afe357d
piccolojnr <rahimdaud246@gmail.com>
2024-02-28 00:39:55 +0000

    adsddas

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """API blueprint definition for endpoint /api/v1"""
 
+
+from flask import Blueprint
+
+# Create a Blueprint instance with url_prefix
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 # Import routes (index.py is not checked for PEP8)
 import api.v1.views.index
 import api.v1.views.states
@@ -10,9 +16,3 @@ import api.v1.views.users
 import api.v1.views.places
 import api.v1.views.places_reviews
 import api.v1.views.places_amenities
-
-
-from flask import Blueprint
-
-# Create a Blueprint instance with url_prefix
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
